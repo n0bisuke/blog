01d3f6dea6c712822ed77af4f736cad446127413
n0bisuke <rsksound@gmail.com>
2022-12-19 17:31:12 +0900

    update

--- src/server.js
+++ src/server.js
@@ -3,8 +3,4 @@ const app = new Hono()
 
 app.get('/', (ctx) => ctx.text('Hello world, this is Hono!!'))
 
-app.get('/public/*', async (ctx) => {
-    return await ctx.env.ASSETS.fetch(ctx.req)
-})
-
 export default app
